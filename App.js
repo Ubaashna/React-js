0b33a1782297a167cf275bb5d92fc406f3f807ba
Ubaashna <97388817+Ubaashna@users.noreply.github.com>
2023-07-30 09:55:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header title="Rajalakshmi Institute of Technology"/>
+      <Counter/>
+      </div>
+  );
+}
+
+export default App;
---
+++ Counter.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+
+function Counter() {
+
+
+    const [count,setCount] = useState(9);
+
+
+    const handleAdd=()=>{
+        setCount(count+1);
+        console.log(count);
+    }
+    const handleSub=()=>{
+        setCount(count-1);
+        console.log(count);
+    }
+  return (
+    <div>
+        <h2>Counter</h2>
+        <h3>{count}</h3>
+        <button onClick={handleSub} style={{padding:5,backgroundColor:"#045590",margin:4,color:"white"}}>-</button>
+        <button onClick={handleAdd} style={{padding:5,backgroundColor:"#045590",margin:4,color:"white"}}>+</button>
+        </div>
+  )
+}
+
+export default Counter
